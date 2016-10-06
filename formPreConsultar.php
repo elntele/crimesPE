cfeba17742f8c6935759fcd9ec09462b2a110c98
Jorge Candeioas do Nascimento <elntele@gmail.com>
2016-10-06 13:27:45 -0300

    formPreconsultar com checkbox ao inves de radio e com alguns elementos do bootstrap

--- formPreConsultar.php
+++ formPreConsultar.php
@@ -6,50 +6,79 @@
 	<link rel="stylesheet"  href="bootstrap/css/bootstrap.css">
 	<script src="bootstrap/js/jquery-3.1.1.js"></script>
 	<script src="bootstrap/js/bootstrap.js"></script>
+	<!--######################################### -->
+	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
+
+
+	<!--######################################### -->
 
 	<title></title>
 </head>
-</div>
 <body> 
-<h1>selecione uma ou mais caracteristicas você quer consultar o crime</h1>
-<div>
-
-	<form>
-		<h3> consultar por cidades? </h3>
-		<input type="radio" name="cidade">
-	</form>
-	<form>
-		<h3> consultar por nome? </h3>
-		<input type="radio" name="nome_vitima">
-	</form>
-<form>
-		<h3> consultar por data de ocorrência? </h3>
-		<input type="radio" name="data_ocorrencia">
-	</form>
-<form>
-		<h3> consultar por idade? </h3>
-		<input type="radio" name="idade">
-	</form>
-	<form>
-		<h3> consultar por sexo? </h3>
-		<input type="radio" name="sexo">
-	</form>
-<form>
-		<h3> consultar por objeto utilizado? </h3>
-		<input type="radio" name="objeto_utilizado">
-	</form>
-<form>
-		<h3> consultar por tipo de crime? </h3>
-		<input type="radio" name="tipo_crime">
-	</form>
-<form>
-		<h3> consultar por quantidade de vítimas? </h3>
-		<input type="radio" name="cidade">
-	</form>
+	<div class="container">
+		<div class="jumbotron">
+		  <h2>CONTROLE DE CONSULTA</h2>
+		  <p>selecione uma ou mais caracteristicas pelas quais você quer consultar o crime:</p>
+		    <form>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="cidade">Consultar por cidades</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="nome_vitima">consultar por nome?</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="data_ocorrencia" >consultar por data de ocorrência?</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="idade" >consultar por idade?</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="sexo" >consultar por sexo?</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="objeto_utilizado" >consultar por objeto utilizado?</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="tipo_crime" > consultar por tipo de crime?</label>
+			    </div>
+			    <div class="checkbox">
+			      <label><input type="checkbox" value="qtd_vitima" >consultar por quantidade de vítimas?</label>
+			    </div>
+	  		</form>
+	  	</div>
+	</div>
 
 
-
-
+	<footer class="section section-primary"> 
+		<div class="container"> 
+			<div class="row"> 
+				<div class="col-sm-6"> 
+					<h1>Sistema de denúncia de crimes</h1> 
+					<p>Todos os direitos reservados</p>
+				</div>
+				<div class="col-sm-6"> <p class="text-info text-right"> <br><br></p>
+					<div class="row"> 
+						<div class="col-md-12 hidden-lg hidden-md hidden-sm text-left"> 
+							<a href="#"><i class="fa fa-3x fa-fw text-inverse fa-apple"></i></a> 
+							<a href="#"><i class="fa fa-3x fa-fw text-inverse fa-android"></i></a> 
+							<a href="#"><i class="fa fa-3x fa-fw text-inverse fa-windows"></i></a> 
+							<a href="#"><i class="fa fa-3x fa-fw hub text-inverse fa-linux"></i></a> 
+						</div>
+					</div>
+					<div class="row"> 
+						<div class="col-md-12 hidden-xs text-right"> 
+							<a href="#"><i class="fa fa-3x fa-fw text-inverse fa-apple"></i></a> 
+							<a href="#"><i class="fa fa-3x fa-fw text-inverse fa-android"></i></a> 
+							<a href="#"><i class="fa fa-3x fa-fw text-inverse fa-windows"></i></a> 
+							<a href="#"><i class="fa fa-3x fa-fw hub text-inverse fa-linux"></i></a> 
+						</div>
+					</div>					
+				</div>
+			</div>
+		</div>
+	</footer>
 
 </body>
-VALUES ('$_POST[cidade]', '$_POST[data_ocorrencia]', '$_POST[idade]', '$_POST[nome_vitima]', '$_POST[objeto_utilizado]', '$_POST[qtd_vitimas]', '$_POST[sexo]', '$_POST[tipo_crime]')";
