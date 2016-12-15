d91528c1aab48a95bd4465591d9dc1f455a8dfa8
Jorge Candeias do Nascimento <elntele@gmail.com>
2016-12-15 17:30:06 -0300

    primeira va

--- consultarCrime.php
+++ consultarCrime.php
@@ -18,8 +18,21 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-    echo "<table border='1' align=center>";
+    echo "<!DOCTYPE html>";
+    echo"<html>";
+    echo"<head>";
+    	echo"<title>Bootstrap Example</title>";
+	 	 echo"<meta charset='utf-8'>";
+  		echo"<meta name='viewport' content='width=device-width, initial-scale=1'>";
+  		echo"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>";
+  		echo"<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>";
+  		echo"<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>";
+    echo"</head>";
+    echo"<body>";
+    echo "<div class='container'>";
+    echo "<table class='table table-striped'>";
     echo "<tr><td>CIDADE</td> <td>DATA DA OCORRÊNCIA</td><td>IDADE</td><td>NOME DA VíTIMA</td><td>OBJETO UTILIZADO</td><td>QTD DE VÍTIMAS</td> <td>SEXO</td> <td>TIPO DE CRIME</td> </tr>";
+
     while($row = mysqli_fetch_assoc($result)) {
          
       echo " <tr> <td>" . $row["cidade"] ."</td>" . "<td>". $row["data_ocorrencia"]. "</td>" ."<td>". $row["idade"]."</td>"."<td>". $row["nome_vitima"]. "</td>"."<td>". $row["objeto_utilizado"]."</td>". "<td>". $row["qtd_vitimas"]. "</td>"."<td>". $row["sexo"]."</td>"."<td>" .$row["tipo_crime"]."</td></tr>" ;
@@ -30,4 +43,8 @@ if (mysqli_num_rows($result) > 0) {
 }
 
 mysqli_close($conn);
+  echo "</div >";  
+ echo   "</body>";
+ echo  "</html>";
+    
 ?>
