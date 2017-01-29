708a9c2f30e54ff2ba0fe8f6b39479d7945d0cdb
Jorge Candeias do Nascimento <elntele@gmail.com>
2017-01-28 21:19:59 -0300

    inicio do madificacao para segunda va mais o banco

---
+++ formlogin.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" http-equiv="X-UA-Compatible" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet"  href="bootstrap/css/bootstrap.css">
+		<script src="bootstrap/js/jquery-3.1.1.js"></script>
+		<script src="bootstrap/js/bootstrap.js"></script>
+		<script type="text/javascript" src="consultarCrimeAjax.js"></script>
+		<script src="bootstrap/js/validator.js"></script>
+
+		<script>
+			
+			function toUpper(input) {
+				// body...
+				input.value = input.value.toUpperCase();
+			}
+
+		</script>
+		<title></title>
+	</head>
+	<body> 
+		<div class="page-header text-center">
+    		<h2>INFORME USUÁRIO E SENHA</h2>
+    	</div>
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-sm-6">
+ 
+			    <!--########################-->	
+
+			<!--    /* na primeira linha usaremos o “action=ope.php” para que o formulário repasse as informações para a pagina ope.php */
+<form method="post" action="ope.php" id="formlogin" name="formlogin" >
+<fieldset id="fie">
+<legend>LOGIN</legend><br />
+<label>NOME : </label>
+// o campo “name” dentro do input e importante, pois será ele que armazenará os dados digitados . 
+<input type="text" name="login" id="login"  /><br />
+<label>SENHA :</label>
+<input type="password" name="senha" id="senha" /><br />
+<input type="submit" value="LOGAR	"  />
+</fieldset>
+</form>
+-->
+
+			     		<div class= "form-group"  >
+			    			
+			    				<form method="post" action="login.php" id="formlogin" name="formlogin">
+			    				<fieldset id="fie">
+			    					<legend>LOGIN</legend><br />
+			    					<label class="control-label">USUÁRIO</label>
+			    	    			<input class="form-control input-sm" type="text" name="nome_usuario" id="nome_usuario" placeholder="Informe o nome do usuário">
+			    	    		
+			    					<label class="control-label">SENHA</label>
+			    					<br>
+			    	    			<input class="form-control input-sm" type="password" name="senha" id="senha" placeholder="Informe a sua senha">
+			    	    			<input type="submit" value="Logar"/>
+			    	    			<br>	
+			    	    			<a href="formPreConsultarCrime.php" class="btn btn-primary"  role="button">voltar</a>
+
+									<!--<input type="button" onclick="buscarCrimes()" class="btn btn-primary">Consultar</input>
+									<button  onclick="buscarCrimes()" class="btn btn-primary">Entrar</button>	-->
+			    			
+
+			    	    		</form>
+			    			</fieldset>
+
+			    			</div>
+
+							<div id= "resultado">
+								
+							</div>
+
+			
+	</body>
+</html>
